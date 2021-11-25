5a16b66fae101e1ac20bb8c3752000d9fd5066d4
ariel <ariroffe@gmail.com>
2021-11-25 13:57:40 -0300

    More NPCs and animations

--- scenes/software.js
+++ scenes/software.js
@@ -9,6 +9,8 @@ export class SoftwareScene extends BaseScene {
   preload() {
     // this.load.image("InsideTiles", "./assets/prod/tilesets_and_maps/poke_inside_converted.png");
     this.load.image("InsideTiles", "./assets/prod/tilesets_and_maps/poke_inside_converted_extruded.png");
+	this.load.spritesheet('computer', 'assets/prod/anims/computer.png', { frameWidth: 32, frameHeight: 64 });
+	this.load.spritesheet('bigcomputer', 'assets/prod/anims/bigcomputer.png', { frameWidth: 128, frameHeight: 96 });
     this.load.tilemapTiledJSON("SoftwareMap", "./assets/prod/tilesets_and_maps/software.json");
   }
 
@@ -22,6 +24,28 @@ export class SoftwareScene extends BaseScene {
     // On scene switch (after entering through the door) display the walking UP animation
     this.events.on('create', () => {this.player.anims.play("ariel-back-walk", true)}, this);
     this.events.on('wake', () => {this.player.anims.play("ariel-back-walk", true)}, this);
+	
+	// Small computer animation
+    this.anims.create({
+      key: "computer-anim",
+      frameRate: 2,
+      frames: this.anims.generateFrameNumbers("computer", { start: 0, end: 1 }),
+      yoyo: false,
+      repeat: -1
+    });
+    let computer = this.add.sprite(880, 64, "computer");
+    computer.play("computer-anim");
+	
+	// Big computer animation
+    this.anims.create({
+      key: "bigcomputer-anim",
+      frameRate: 2,
+      frames: this.anims.generateFrameNumbers("bigcomputer", { start: 0, end: 1 }),
+      yoyo: false,
+      repeat: -1
+    });
+    let bigcomputer = this.add.sprite(480, 48, "bigcomputer");
+    bigcomputer.play("bigcomputer-anim");
 
     this.collide_with_world();  // Has to be called after the rest of the colliders are defined
   }
