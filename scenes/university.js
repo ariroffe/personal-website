d542ba085f2cbc8da03341c776db0681bf18c539
ariel <ariroffe@gmail.com>
2021-11-25 12:00:17 -0300

    Fountain animation in university scene

--- scenes/university.js
+++ scenes/university.js
@@ -9,6 +9,7 @@ export class UniversityScene extends BaseScene {
   preload() {
     // this.load.image("InsideTiles", "./assets/prod/tilesets_and_maps/poke_inside_converted.png");
     this.load.image("InsideTiles", "./assets/prod/tilesets_and_maps/poke_inside_converted_extruded.png");
+    this.load.spritesheet('fountain', 'assets/prod/anims/fountain.png', { frameWidth: 64, frameHeight: 64 });
     this.load.tilemapTiledJSON("UniversityMap", "./assets/prod/tilesets_and_maps/university.json");
   }
 
@@ -22,6 +23,20 @@ export class UniversityScene extends BaseScene {
     // On scene switch (after entering a door) display the walking up animation
     this.events.on('create', () => {this.player.anims.play("ariel-back-walk", true)}, this);
     this.events.on('wake', () => {this.player.anims.play("ariel-back-walk", true)}, this);
+
+    // Fountain animation
+    this.anims.create({
+      key: "fountain-anim",
+      frameRate: 5,
+      frames: this.anims.generateFrameNumbers("fountain", { start: 1, end: 3 }),
+      yoyo: false,
+      repeat: -1
+    });
+    let fountain = this.add.sprite(928, 608, "fountain");
+    fountain.playReverse("fountain-anim");
+    // It is sitting exactly on top of the Tiled (fixed) fountain, so no need for a collider
+    // this.physics.add.existing(fountain, true);
+    // this.physics.add.collider(this.player, fountain);
 
     this.collide_with_world();  // Has to be called after the rest of the colliders are defined
   }
