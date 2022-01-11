c4ca787b4abd516617edd14dc89f2faa8e490cf9
ariel <ariroffe@gmail.com>
2022-01-11 20:11:01 -0300

    Static site navigation bar (working on mobile)

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
+function mobileOrTablet() {
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+}
+let mobile = mobileOrTablet();
+
 // ------------------------------
 // Language selection
 
@@ -34,17 +41,18 @@ light_mode_button.addEventListener("click", function() {
 // Nav navigation
 
 // ul translateY
-let displaceY = 39;
-let overNav = false;
+let displaceY = 38;
 const nav_ul = document.getElementById("navbar").firstElementChild;
-nav_ul.addEventListener("pointerover", () => {
-    overNav = true;
-    nav_ul.style.transform = "translateY(0)";
-})
-nav_ul.addEventListener("pointerout", () => {
-    overNav = false;
-    nav_ul.style.transform = "translateY(-" + displaceY  +"px)";
-})
+if (!mobile) {
+    nav_ul.addEventListener("pointerover", () => nav_ul.style.transform = "translateY(0)");
+    nav_ul.addEventListener("pointerout", () => nav_ul.style.transform = "translateY(-" + displaceY  +"px)");
+} else {
+    nav_ul.addEventListener("pointerdown", (ev) => {
+        nav_ul.style.transform = "translateY(0)"
+        ev.stopPropagation();  // So that the document event below does not fire
+    });
+    document.addEventListener("pointerdown", () => nav_ul.style.transform = "translateY(-" + displaceY  +"px)")
+}
 
 // Link click
 const links = Array.from(document.getElementsByClassName("nav-link"));
@@ -53,12 +61,18 @@ let sections = [];
 let section_ids = [];   // for the next bit. Do it dinamically so we don't have to change this if we add sections...
 links.forEach((link) => {
     link.addEventListener("click", function(ev) {
+        // Prevent link clicking if the menu is not expanded (useful for mobile)
+        if (mobile && nav_ul.parentNode.offsetHeight <= 60) {
+            ev.preventDefault();
+            return false
+        }
+
         // Cancel the other links active status
         links.forEach((other_link) => other_link.classList.remove("active"));
 
         // Give it to this one
         link.classList.add("active");
-        displaceY = links.indexOf(link) * 39;  // do not move the ul just yet, set the displace for when the user mouseouts
+        displaceY = links.indexOf(link) * 38;  // do not move the ul just yet, set the displace for when the user mouseouts
     })
 
     // Fill the section and section_id arrays
@@ -86,7 +100,7 @@ function get_current_section_id() {
     return section_ids[offsets.indexOf(get_current_offset())];
 }
 
-let prev_section = "#intro";
+let prev_section = "#";  // So that at first initialization of site it always hits the conditional
 
 // The following is not very efficient, but since the sections can expand and contract other approaches were way more complicated
 function setActiveLink() {
@@ -99,8 +113,8 @@ function setActiveLink() {
         links.forEach((other_link) => other_link.classList.remove("active"));  // Clear all links active status
         // The +1 in the next two lines is bc links includes the Game link
         links[new_active_link_index + 1].classList.add("active");  // Set the new link as the active one
-        displaceY = (new_active_link_index + 1) * 39;  // Correct the global var so that if you enter and leave menu, it stays where it must
-        if (!overNav) nav_ul.style.transform = "translateY(-" +  displaceY  +"px)";  // Move the navbar up
+        displaceY = (new_active_link_index + 1) * 38;  // Correct the global var so that if you enter and leave menu, it stays where it must
+        if (nav_ul.parentNode.offsetHeight <= 60)  nav_ul.style.transform = "translateY(-" +  displaceY  +"px)";  // Move the navbar up only if not expanded
         prev_section = current_section;
     }
 }
